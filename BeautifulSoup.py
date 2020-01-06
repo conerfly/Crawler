8d4e2cec284cb510320d3ac34a7cdf875001a4c7
Johnny <conerfly@hotmail.com>
2020-01-06 23:49:17 +0800

    Changed BeautifulSoup.py

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -6,7 +6,7 @@ try:
     r.raise_for_status()
     r.encoding = r.apparent_encoding
     soup = BeautifulSoup(r.text, 'html.parser')
-    # print(soup.prettify())
-    print(type(soup.head.attrs))   #查看soup标签的类型，beautifulsoup类一共有五种类型
+    print(soup.prettify())
+    # print(type(soup.head.attrs))   #查看soup标签的类型，beautifulsoup类一共有五种类型
 except:
     print("error")
